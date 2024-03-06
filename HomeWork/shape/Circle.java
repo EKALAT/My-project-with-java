838075dd51d45749eea75c0d126c70d0ed074dcc
EKALAT <koongxd66@gmail.com>
2024-03-06 16:30:20 +0700

    First commit

---
+++ HomeWork/shape/Circle.java
@@ -0,0 +1,14 @@
+package shape;
+
+public class Circle implements IShape {
+    double radius;
+    public Circle(double radius){
+        this.radius = radius;
+    }
+    public double getPerimeter(){
+        return 2 * Math.PI*radius;
+    }
+    public double getArea(){
+        return Math.PI*radius*radius;
+    }
+}
